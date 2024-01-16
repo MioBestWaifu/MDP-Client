741302b4b8ad592ca55378d9664a4a1ce5f147e1
MioBestWaifu <Fenix1603@hotmail.com>
2024-01-16 13:42:16 -0300

    Implementando reviews list

--- MDP/src/app/components/structure/reviews-list/reviews-list.component.ts
+++ MDP/src/app/components/structure/reviews-list/reviews-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BaseComponent } from '../../base/base.component';
+import { Interaction } from '../../../models/interaction';
 
 @Component({
   selector: 'mdp-reviews-list',
-  
-  
   templateUrl: './reviews-list.component.html',
   styleUrl: './reviews-list.component.scss'
 })
-export class ReviewsListComponent {
-
+export class ReviewsListComponent extends BaseComponent{
+  @Input() vertical:boolean = false
+  @Input() reviews!:Interaction[]
 }
--- MDP/src/app/models/user.ts
+++ MDP/src/app/models/user.ts
@@ -1,12 +1,13 @@
 export class User {
-    email:string | undefined
-    password:string | undefined
-    nickname!:string
-    cardImgUrl!:string
-    mainImgUrl!:string
-    otherImgUrls:string[] | undefined
-    country!:string
-    description:string | undefined
-    birthday!:Date
-    gender:string | undefined
+    id!:number;
+    email:string | undefined;
+    password:string | undefined;
+    nickname!:string;
+    cardImgUrl!:string;
+    mainImgUrl!:string;
+    otherImgUrls:string[] | undefined;
+    country!:string;
+    description:string | undefined;
+    birthday!:Date;
+    gender:string | undefined;
 }
