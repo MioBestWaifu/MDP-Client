9e2ef523bcb344416fcf902aebc37b538ed32d0f
MioBestWaifu <Fenix1603@hotmail.com>
2024-01-18 23:56:08 -0300

    mock artifact

--- MDP/src/app/models/artifacts/artifact.ts
+++ MDP/src/app/models/artifacts/artifact.ts
@@ -16,4 +16,23 @@ export class Artifact {
     mainParticipant!: string;
     averageRating!: number;
     releaseDate: Date | undefined;
+
+    static readonly testArtifact:Artifact = {
+        id: 1,
+        shortName: 'Test Artifact',
+        fullName: 'Test Artifact Full Name',
+        otherNames: ['Test Artifact Other Name 1', 'Test Artifact Other Name 2'],
+        description: 'This is a test artifact',
+        media: 'Test Media',
+        categories: ['Test Category 1', 'Test Category 2'],
+        targetDemographics: ['Test Demographic 1', 'Test Demographic 2'],
+        ageRating: 'Test Age Rating',
+        cardImgUrl: 'https://aishow.blob.core.windows.net/images/2.png',
+        mainImgUrl: 'https://aishow.blob.core.windows.net/images/2.png',
+        otherImgUrls: ['https://aishow.blob.core.windows.net/images/2.png', 'https://aishow.blob.core.windows.net/images/2.png'],
+        mainParticipantRole: 'Test Role',
+        mainParticipant: 'Test Participant',
+        averageRating: 4.5,
+        releaseDate: new Date(2020, 0, 1)
+    }
 }
--- MDP/src/app/models/pages/home-page-model.ts
+++ MDP/src/app/models/pages/home-page-model.ts
@@ -5,4 +5,14 @@ import { BasePageModel } from "./base-page-model"
 export class HomePageModel extends BasePageModel{
     artifacts!:Artifact[]
     newsAndHighlights!:Link[]
+
+    constructor(){
+        super()
+        this.artifacts = []
+
+        for (let i = 0; i < 10; i++) {
+            this.artifacts.push(Artifact.testArtifact)
+        }
+        this.newsAndHighlights = []
+    }
 }
