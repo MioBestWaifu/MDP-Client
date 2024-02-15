7fc170159afb3f5d65a787dca5ff459aeb9b0d3a
OzyHil <117992907+OzyHil@users.noreply.github.com>
2024-02-15 09:22:00 -0300

    Limpeza na SearchBar
    Apagar o texto contido na SearchBar após uma mudança na rota de navegação.

--- MDP/src/app/components/structure/ad-list/ad-list.component.ts
+++ MDP/src/app/components/structure/ad-list/ad-list.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mdp-ad-list',
-  
-  
   templateUrl: './ad-list.component.html',
   styleUrl: './ad-list.component.scss'
 })
 export class AdListComponent {
-
+  
 }
