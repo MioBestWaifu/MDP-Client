17739923ff2f34e3d0c9f2d26adf03c7b58442bf
OzyHil <117992907+OzyHil@users.noreply.github.com>
2024-02-23 18:07:49 -0300

    Removendo da pagina
    remoção do trending da pagina de pesquisa. alem de colocar uma condicional diretiva para mostrar resultados ou nao(caso n hajam um template é gerado dziendo q n ha resultados)

--- MDP/src/app/components/controls/search-filter/search-filter.component.ts
+++ MDP/src/app/components/controls/search-filter/search-filter.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../../base/base.component';
 
 @Component({
   selector: 'mdp-search-filter',
-  
-  
+
+
   templateUrl: './search-filter.component.html',
   styleUrl: './search-filter.component.scss'
 })
-export class SearchFilterComponent {
-
+export class SearchFilterComponent extends BaseComponent {
+  canShow: boolean = true;
 }
--- MDP/src/app/components/structure/ad-list/ad-list.component.ts
+++ MDP/src/app/components/structure/ad-list/ad-list.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Artifact } from '../../../models/artifacts/artifact';
+import { BaseComponent } from '../../base/base.component';
 
 @Component({
   selector: 'mdp-ad-list',
   templateUrl: './ad-list.component.html',
   styleUrl: './ad-list.component.scss'
 })
-export class AdListComponent {
-  
+
+export class AdListComponent extends BaseComponent {
+  @Input() artifact: Artifact = Artifact.testArtifact;
 }
