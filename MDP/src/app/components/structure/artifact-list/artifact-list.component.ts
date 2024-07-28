b3b8a7436f89a0677156ff5d69aa99738e1f051f
OzyHil <117992907+OzyHil@users.noreply.github.com>
2024-07-28 13:33:07 -0300

    starlist and artifact card

--- MDP/src/app/components/structure/artifact-list/artifact-list.component.ts
+++ MDP/src/app/components/structure/artifact-list/artifact-list.component.ts
@@ -9,39 +9,39 @@ import { Dictionary } from '../../../classes/dictionary';
   styleUrl: './artifact-list.component.scss'
 })
 export class ArtifactListComponent extends BaseComponent implements OnChanges{
-  @Input() vertical:boolean = false;
+  // @Input() vertical:boolean = false;
   @Input() artifacts!:Artifact[];
-  artifactRows!:Dictionary<number,Artifact[]>;
-  effectiveMargin!:number;
+  // artifactRows!:Dictionary<number,Artifact[]>;
+  // effectiveMargin!:number;
 
   override ngOnInit(): void {
       super.ngOnInit();
-      this.common.barToggleEvent.subscribe(event => {
-        this.RecalculateRows();
-      });
+      // this.common.barToggleEvent.subscribe(event => {
+      //   this.RecalculateRows();
+      // });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.RecalculateRows();
+    // this.RecalculateRows();
   }
 
-  RecalculateRows() {
-    let availaibleWidth:number = this.elementRef.nativeElement.clientWidth;
-    /* console.log("Recalculating rows " + availaibleWidth); */
+  // RecalculateRows() {
+  //   let availaibleWidth:number = this.elementRef.nativeElement.clientWidth;
+  //   /* console.log("Recalculating rows " + availaibleWidth); */
 
-    let rowItems = 1;
-    while (rowItems * 150 <= availaibleWidth - 380) {
-      rowItems++;
-    }
+  //   let rowItems = 1;
+  //   while (rowItems * 150 <= availaibleWidth - 380) {
+  //     rowItems++;
+  //   }
 
-    this.artifactRows = new Dictionary<number, Artifact[]>();
-    let index = 0;
-    while (index < this.artifacts.length) {
-      const row = this.artifacts.slice(index, index + rowItems);
-      this.artifactRows.Add(index, row);
-      index += rowItems;
-    }
+  //   this.artifactRows = new Dictionary<number, Artifact[]>();
+  //   let index = 0;
+  //   while (index < this.artifacts.length) {
+  //     const row = this.artifacts.slice(index, index + rowItems);
+  //     this.artifactRows.Add(index, row);
+  //     index += rowItems;
+  //   }
 
-  }
+  // }
   
 }
