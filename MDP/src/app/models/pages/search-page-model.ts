98766c57dcc9a75fef76ca1699236ec6b74e8cfd
MioBestWaifu <Fenix1603@hotmail.com>
2024-07-31 14:33:05 -0300

    Update search-page-model.ts

--- MDP/src/app/models/pages/search-page-model.ts
+++ MDP/src/app/models/pages/search-page-model.ts
@@ -2,13 +2,5 @@ import { Artifact } from "../artifacts/artifact";
 import { BasePageModel } from "./base-page-model";
 
 export class SearchPageModel extends BasePageModel{
-    artifacts!:Artifact[] | undefined
-
-    constructor(){
-        super()
-        /* this.artifacts = []
-        for (let i = 0; i < 10; i++) {
-            this.artifacts.push(Artifact.testArtifact)
-        } */
-    }
+    artifacts!:Artifact[] | undefined;
 }
