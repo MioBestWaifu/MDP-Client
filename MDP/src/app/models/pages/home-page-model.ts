116667b6679877c85d61a34717ab008f765a48ac
MioBestWaifu <Fenix1603@hotmail.com>
2024-07-31 14:15:06 -0300

    Update home-page-model.ts

--- MDP/src/app/models/pages/home-page-model.ts
+++ MDP/src/app/models/pages/home-page-model.ts
@@ -1,21 +1,9 @@
 import { Artifact } from "../artifacts/artifact"
+import { News } from "../information/news"
 import { Link } from "../link"
 import { BasePageModel } from "./base-page-model"
 
 export class HomePageModel extends BasePageModel{
     artifacts!:Artifact[]
-    newsAndHighlights!:Link[]
-
-    constructor(){
-        super()
-        /* this.artifacts = []
-
-        for (let i = 0; i < 15; i++) {
-            this.artifacts.push(Artifact.testArtifact)
-        }
-        this.newsAndHighlights = []
-        this.newsAndHighlights.push(Link.testCarouselItem0)
-        this.newsAndHighlights.push(Link.testCarouselItem1)
-        this.newsAndHighlights.push(Link.testCarouselItem2) */
-    }
+    newsAndHighlights!:News[]
 }
